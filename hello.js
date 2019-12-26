b1d66498a5bb8f0da0fdf7cd9133a1ea17905106
m-serrano <slinux.ve@gmail.com>
2019-12-26 16:30:49 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $.ajax({
-        url: "http://52.154.167.44/customer/api/customers",
-        headers: { 'Ocp-Apim-Subscription-Key': '4f6bce226bea4eaf9bf6f2ecb2bb1916;product=unlimited' }
+        url: "http://40.71.232.166/customer/api/customers",
+        headers: { 'Ocp-Apim-Subscription-Key': '8c830bf9a18643ddb17c5df7bb6e82af;product=unlimited' }
     }).then(function(data) {
        $('.clientes').append(data[Math.floor(Math.random()*3)]);
     });
@@ -9,8 +9,8 @@ $(document).ready(function() {
 
 /* $(document).ready(function() {
     $.ajax({
-        url: "http://52.141.219.120/products/api/products",
-        headers: { 'Ocp-Apim-Subscription-Key': '9db32e4fed844af0aac4537ed2f784c8;product=unlimited' }
+        url: "http://40.71.232.166/products/api/products",
+        headers: { 'Ocp-Apim-Subscription-Key': '8c830bf9a18643ddb17c5df7bb6e82af;product=unlimited' }
     }).then(function(data) {
        $('.productos').append(data[Math.floor(Math.random()*3)]);
     });
